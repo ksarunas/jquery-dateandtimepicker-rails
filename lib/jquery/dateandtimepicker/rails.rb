f9b7e0de1261e84fc429a272b1ba5e78de998725
ksarunas <sarunas.kujalis@vinted.com>
2015-08-19 14:40:29 +0300

    Initial commit. jQuery datetimepicker version 2.4.5

---
+++ lib/jquery/dateandtimepicker/rails.rb
@@ -0,0 +1,10 @@
+require 'jquery/dateandtimepicker/rails/version'
+
+module Jquery
+  module Dateandtimepicker
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/jquery/dateandtimepicker/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Dateandtimepicker
+    module Rails
+      VERSION = '1.0.0'
+    end
+  end
+end
